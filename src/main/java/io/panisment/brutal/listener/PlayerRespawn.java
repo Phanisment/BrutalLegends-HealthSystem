0def911c5038fc48b124ac06651076f7432cb430
Agil <166532433+Phanisment@users.noreply.github.com>
2024-08-26 22:34:17 +0700

    update src/main/java/io/panisment/brutal/listener/PlayerRespawn.java

--- src/main/java/io/panisment/brutal/listener/PlayerRespawn.java
+++ src/main/java/io/panisment/brutal/listener/PlayerRespawn.java
@@ -1,1 +1,22 @@
+package io.phanisment.brutal.listener;
 
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerRespawnEvent;
+
+public class PlayerRespawn implements Listener {
+	@EventHandler
+	public void onPlayerRespawn(PlayerRespawnEvent event) {
+		Player player = event.getPlayer();
+		AttributeInstance health = player.getAttribute(Attribute.GENERIC_MAX_HEALTH);
+		double baseHealth = health.getBaseValue();
+		if (baseHealth > 1.0) {
+			health.setBaseValue(baseHealth - 2.0);
+		} else {
+			health.setBaseValue(2.0);
+		}
+	}
+}
