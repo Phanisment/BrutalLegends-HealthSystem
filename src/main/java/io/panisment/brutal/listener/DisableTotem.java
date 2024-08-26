579b354f093a07b4cb6612b517770fcdc60a275a
Agil <166532433+Phanisment@users.noreply.github.com>
2024-08-26 22:41:44 +0700

    update src/main/java/io/panisment/brutal/listener/DisableTotem.java

---
+++ src/main/java/io/panisment/brutal/listener/DisableTotem.java
@@ -0,0 +1,9 @@
+package io.phanisment.brutal.listener;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityResurrectEvent;
+
+public class DisableTotem {
+	
+}
