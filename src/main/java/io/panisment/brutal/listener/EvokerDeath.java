e971b97a10332f0e9de606279cccd55576af2f27
Agil <166532433+Phanisment@users.noreply.github.com>
2024-08-26 22:33:06 +0700

    update src/main/java/io/panisment/brutal/listener/EvokerDeath.java

--- src/main/java/io/panisment/brutal/listener/EvokerDeath.java
+++ src/main/java/io/panisment/brutal/listener/EvokerDeath.java
@@ -1,1 +1,26 @@
+package io.phanisment.brutal.listener;
 
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.event.entity.EntityDeathEvent;
+
+import java.util.Random;
+
+public class EvokerDeath implements Listener {
+	private final Random random = new Random();
+	
+	@EventHandler
+	public void onEvokerDeath(EntityDeathEvent event) {
+		if (event.getEntityType() == EntityType.EVOKER) {
+			event.getDrops().clear();
+			double dropChance = 0.10;
+			if (random.nextDouble() <= dropChance) {
+				ItemStack totem = new ItemStack(Material.TOTEM_OF_UNDYING);
+				event.getDrops().add(totem);
+			}
+		}
+	}
+}
