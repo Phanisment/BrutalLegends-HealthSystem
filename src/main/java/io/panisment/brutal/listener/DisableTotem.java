678c77fe07b623d6012265c58678efe98e681f91
Agil <166532433+Phanisment@users.noreply.github.com>
2024-08-28 00:24:53 +0700

    update src/main/java/io/panisment/brutal/listener/DisableTotem.java

--- src/main/java/io/panisment/brutal/listener/DisableTotem.java
+++ src/main/java/io/panisment/brutal/listener/DisableTotem.java
@@ -5,8 +5,17 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityResurrectEvent;
 
 public class DisableTotem implements Listener{
+	private final HealthSystem plugin;
+	
+	public DisableTotem(HealthSystem plugin) {
+		this.plugin = plugin;
+	}
+	
 	@EventHandler
 	public void onResurrect(EntityResurrectEvent event) {
-		event.setCancelled(true);
+		Boolean DISABLE_TOTEM = this.plugin.getConfig().getBoolean("Disable_Vanilla_System.Disable_Totem_Function");
+		if (DISABLE_TOTEM == true) {
+			event.setCancelled(true);
+		}
 	}
 }
