40502a8586266656d2190595e65719711d8c6cd4
Agil <166532433+Phanisment@users.noreply.github.com>
2024-08-30 20:49:23 +0700

    update src/main/java/io/panisment/brutal/listener/PlayerRespawn.java

--- src/main/java/io/panisment/brutal/listener/PlayerRespawn.java
+++ src/main/java/io/panisment/brutal/listener/PlayerRespawn.java
@@ -8,6 +8,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.BanList;
 import org.bukkit.BanList.Type;
+import org.bukkit.Bukkit;
 
 import io.phanisment.brutal.HealthSystem;
 
@@ -36,8 +37,7 @@ public class PlayerRespawn implements Listener {
 	}
 	
 	private void banPlayer(Player player) {
-		BanList banList = this.plugin.getServer().getBanList(BanList.Type.NAME);
-		banList.addBan(player.getName(), "You're banned form server because your health is empty.", null, null);
+		Bukkit.getServer().getBanList(BanList.Type.NAME).addBan(player.getName(), "You're banned form server because your health is empty.", null, null);
 		player.kickPlayer("You're banned form server because your health is empty.");
 	}
 }
