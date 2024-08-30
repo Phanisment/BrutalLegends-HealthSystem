5d2a33f6a32d6c587f205aec1a9ec93ae398c38b
Agil <166532433+Phanisment@users.noreply.github.com>
2024-08-30 16:25:54 +0700

    update src/main/java/io/panisment/brutal/listener/PlayerInteraction.java

--- src/main/java/io/panisment/brutal/listener/PlayerInteraction.java
+++ src/main/java/io/panisment/brutal/listener/PlayerInteraction.java
@@ -2,6 +2,7 @@ package io.phanisment.brutal.listener;
 
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
+import org.bukkit.ChatColor;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.EntityEffect;
@@ -13,29 +14,55 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
+import java.util.HashMap;
+import java.util.UUID;
+
+
 import io.phanisment.brutal.HealthSystem;
 
 public class PlayerInteraction implements Listener {
 	private final HealthSystem plugin;
+	private final HashMap<UUID, integer> usedAmount;
 	
 	public PlayerInteraction(HealthSystem plugin) {
 		this.plugin = plugin;
+		this.usedAmount = new HashMap<>();
 	}
 	
 	@EventHandler
 	public void onPlayerInteract(PlayerInteractEvent event) {
-		Double MAX_HEALTH = this.plugin.getConfig().getDouble("Custom_System.Totem_of_Undying.Max_Health");
+		double MAX_HEALTH = this.plugin.getConfig().getDouble("Custom_System.Totem_of_Undying.Max_Health");
 		if (event.getHand() == EquipmentSlot.HAND && event.getItem().getType() == Material.TOTEM_OF_UNDYING && MAX_HEALTH != null) {
 			if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
 				Player player = event.getPlayer();
 				ItemStack item = event.getItem();
 				AttributeInstance health = player.getAttribute(Attribute.GENERIC_MAX_HEALTH);
 				double baseHealth = health.getBaseValue();
+				
 				if (baseHealth < MAX_HEALTH) {
-					player.getInventory().removeItem(item);
-					player.playEffect(EntityEffect.TOTEM_RESURRECT);
-					health.setBaseValue(baseHealth + 2.0);
-				} 
+					UUID playerUUID = player.getUniqueId();
+					int used = usedAmount.getOrDefault(playerUUID, 0);
+					int maxUse = this.plugin.getConfig().getInt("Custom_System.Totem_of_Undying.Max_Usage_Per_Hours");
+					
+					if (maxUse != 0) {
+						if (used < maxUse) {
+							usedAmount.put(playerUUID, used + 1);
+							player.getInventory().setItemInMainHand(null);
+							player.playEffect(EntityEffect.TOTEM_RESURRECT);
+							double addHealth = this.plugin.getConfig().getDouble("Custom_System.Totem_of_Undying.Add_Health");
+							if (addHealth == null || addHealth < 0.0) {
+								addHealth = 2.0;
+							}
+							health.setBaseValue(baseHealth + addHealth);
+						} else {
+							String MAX_USED_MESSAGE = this.plugin.getConfig().getString("System_Message.Max_Usage_Totem");
+							player.sendMessage(ChatColor.translateAlternateColorCodes('&', ))
+						}
+					}
+				} else if (MAX_MESSAGE != null) {
+					String MAX_MESSAGE = this.plugin.getConfig().getString("System_Message.Max_Use_Totem");
+					player.sendMessage(ChatColor.translateAlternateColorCodes('&', MAX_MESSAGE));
+				}
 			}
 		}
 	}
