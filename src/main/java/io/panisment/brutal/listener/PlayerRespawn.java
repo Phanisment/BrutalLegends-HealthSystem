59d85e97c86a9271fadfd527bbca413b7feed8f9
Agil <166532433+Phanisment@users.noreply.github.com>
2024-08-30 07:45:59 +0700

    update src/main/java/io/panisment/brutal/listener/PlayerRespawn.java

--- src/main/java/io/panisment/brutal/listener/PlayerRespawn.java
+++ src/main/java/io/panisment/brutal/listener/PlayerRespawn.java
@@ -7,16 +7,27 @@ import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
+import io.phanisment.brutal.HealthSystem;
+
 public class PlayerRespawn implements Listener {
+	private final HealthSystem plugin;
+	
+	public RespawnListener(HealthSystem plugin) {
+		this.plugin = plugin;
+	}
+	
 	@EventHandler
 	public void onPlayerRespawn(PlayerRespawnEvent event) {
-		Player player = event.getPlayer();
-		AttributeInstance health = player.getAttribute(Attribute.GENERIC_MAX_HEALTH);
-		double baseHealth = health.getBaseValue();
-		if (baseHealth > 1.0) {
-			health.setBaseValue(baseHealth - 2.0);
-		} else {
-			health.setBaseValue(2.0);
+		Boolean ENABLE = this.plugin.getConfig().getBoolean("Custom_System.Health_System.Enable");
+		if (ENABLE = true) {
+			Player player = event.getPlayer();
+			AttributeInstance health = player.getAttribute(Attribute.GENERIC_MAX_HEALTH);
+			double baseHealth = health.getBaseValue();
+			if (baseHealth > 1.0) {
+				health.setBaseValue(baseHealth - 2.0);
+			} else {
+				health.setBaseValue(2.0);
+			}
 		}
 	}
 }
